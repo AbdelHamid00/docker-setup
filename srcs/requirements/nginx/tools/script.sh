f8ba003c1c86c1340da89dc01367af9d001087d3
Abdelhamid Abouzid <aabouzid@c3r5p1.1337.ma>
2023-11-26 10:06:55 +0100

    changes the config file of nginx

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -2,26 +2,4 @@
 
 openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/nginx.key -out /etc/ssl/nginx.crt -sha256 -subj "/CN=MA"
 
-echo 'server {
-    listen 443 ssl;
-    listen [::]:443 ssl;
-
-    server_name aabouzid.42.fr;
-
-    ssl_certificate /etc/ssl/nginx.crt;
-    ssl_certificate_key /etc/ssl/nginx.key;
-
-    ssl_protocols TLSv1.3;
-
-    index index.php;
-    root /var/www/wordpress;
-
-    location ~ [^/]\.php(/|$) { 
-            try_files  = 404;
-            fastcgi_pass  wordpress:9000;
-            include fastcgi_params;
-            fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-        }
-}' > /etc/nginx/sites-available/default
-
 nginx -g "daemon off;"
